4573bb08d84b59c4e84907427ee50aa0d83f6f6a
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-09-04 02:07:27 +0800

    Set up fonts and layout

---
+++ myportfolio/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import '../styles.css';
+</script>
+
+<Navbar />
+<slot />
+<Footer />
--- myportfolio/src/routes/+page.svelte
+++ myportfolio/src/routes/+page.svelte
@@ -2,3 +2,9 @@
     <title>David Genesis Pedeglorio - Portfolio</title>
 </svelte:head>
 
+<script>
+    import '@fontsource-variable/space-grotesk';
+</script>
+
+<h1>Testing</h1>
+<p class="body-3">Hahaha.</p>
