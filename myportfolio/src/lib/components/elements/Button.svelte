6bdf79cd03912c2fb0ab37c437f87e3e2a5f739f
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-09-21 10:55:08 +0800

    Add BlogSection and ActionCard

--- myportfolio/src/lib/components/elements/Button.svelte
+++ myportfolio/src/lib/components/elements/Button.svelte
@@ -1,15 +1,33 @@
 <style>
-    .button {
-        padding: 12px 24px;
-        border-radius: 10px;
-        background: var(--gradient, linear-gradient(166deg, #FF5800 -2.33%, #FF8A00 90.04%));
-        box-shadow: 3px 3px 4px 0px rgba(255, 255, 255, 0.30) inset, -12px -12px 20px 8px #FFF, 12px 12px 20px 3px rgba(198, 175, 162, 0.30);
-        color: var(--white);
-        border: none;
+    .button {      
+      padding: 12px 24px;
+      border-radius: 10px;
+      background: var(--gradient);
+      box-shadow: 3px 3px 4px 0px rgba(255, 255, 255, 0.30) inset, -12px -12px 20px 8px #FFF, 12px 12px 20px 3px rgba(198, 175, 162, 0.30);
+      color: var(--white);
+      border: none;
+      cursor: pointer;
     }
 
     .button-medium {
-        padding: 10px 6px;
+      padding: 10px 6px;
+    }
+
+    .button:hover {
+      background: var(--pantone-orange);
+    }
+
+    .button:active {
+      background: var(--gradient_yellow-orange, linear-gradient(265deg, #FFC635 5.6%, #FF7A00 118.86%));
+    }
+
+    .with-arrow {
+      display: flex;
+      gap: 8px;
+    }
+
+    .with-arrow label {
+      cursor: pointer;
     }
 
 </style>
@@ -34,7 +52,13 @@
     </label>
   </button>
 {:else}
-  <button class="button">
-    <slot />
+  <button class="button button-medium">
+    <div class="with-arrow">
+      <label>
+        <slot />
+      </label>      
+      <img src="/icons/arrow_outward.svg" alt="Arrow Right" height="24px" />
+    </div>
+    
   </button>
 {/if}
--- myportfolio/src/routes/+page.svelte
+++ myportfolio/src/routes/+page.svelte
@@ -5,7 +5,11 @@
 <script>
     import '@fontsource-variable/space-grotesk';
     import AboutMe from '$lib/components/layouts/AboutMe.svelte';
+	import TechStack from '$lib/components/layouts/TechStack.svelte';
+    import BlogSection from '$lib/components/layouts/BlogSection.svelte';
 </script>
 
 <AboutMe />
+<TechStack />
+<BlogSection />
 
