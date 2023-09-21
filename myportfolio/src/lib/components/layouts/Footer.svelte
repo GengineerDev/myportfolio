3d466416f8702fbbedfdca9f487d576389756ed5
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-09-21 09:51:34 +0800

    Add Footer.svelte

--- myportfolio/src/lib/components/layouts/Footer.svelte
+++ myportfolio/src/lib/components/layouts/Footer.svelte
@@ -1,1 +1,62 @@
-<footer>FOOTER</footer>
+<style>
+    footer {
+        padding: 11px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    footer > div {
+        width: 46.6rem;
+    }
+
+    footer h3 {
+        background: var(--gradient);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+    footer p {
+        color: var(--pantone-orange);
+        font-family: 'General Sans Medium', 'Times New Roman', Times, serif;
+        font-size: 16px;
+    }
+
+    .footer-group {
+        margin-bottom: 24px;
+    }
+
+    .footer-social-icons {
+        display: flex;
+        gap: 8px;
+        align-items: center;
+    }
+
+    .footer-social-icons > img {
+        cursor: pointer;
+    }
+</style>
+
+<footer>
+    <img src="/D.png" alt="Logo of David Genesis Pedeglorio" height="96px" /> 
+    <div>
+        <div class="footer-group">
+            <h3 class="subheadings-3">about.me</h3>
+            <p>Tech Stack</p>
+        </div>
+        <h3 class="subheadings-3">works</h3>
+        <p>Projects</p>
+        <p>Experiences</p>
+        <p>Certificates</p>
+        <p>Other Engagements</p>
+    </div>
+    <div>
+        <h3 class="subheadings-3">contact.me</h3>
+        <div class="footer-social-icons">
+            <img src="/icons/fb-dark.svg" alt="Facebook" height="52.92px" />
+            <img src="/icons/ig-dark.svg" alt="Instagram" height="52.92px" />
+            <img src="/icons/linkedin-dark.svg" alt="LinkedIn" height="52.92px" />
+            <img src="/icons/github-dark.svg" alt="GitHub" height="52.92px" />
+        </div>
+    </div>
+</footer>
