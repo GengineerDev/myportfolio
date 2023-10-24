ebe82b1442f3c331ed0ee2ffa03cce53869999a9
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-10-24 16:44:06 +0800

    Create Input.svelte

---
+++ myportfolio/src/lib/components/elements/Input.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let labelText = "label";
+    export let name = "title";
+    export let inputType = "text";
+</script>
+
+<div>
+    <label for={name} class="body-1">{labelText}</label>
+    <input type={inputType} id={name} name={name}/>
+</div>
+
+
+<style>
+    label {
+        font-family: unset;
+        font-weight: unset;
+        display: block;
+        margin-bottom: 8px;
+    }
+
+    input {
+        padding: 16px 24px;
+        border: 1px solid #676767;
+        width: 100%;
+        background: var(--white);
+        font-family: 'General Sans Regular', 'Times New Roman', Times, serif;
+        font-size: 24px;
+        font-weight: 400;
+    }
+
+    div {
+        margin: 16px 0;
+    }
+</style>
