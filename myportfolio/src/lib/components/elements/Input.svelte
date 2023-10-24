cbaca4b82013e40793ee64314fb2c73bac554761
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-10-24 17:24:41 +0800

    Add textarea

--- myportfolio/src/lib/components/elements/Input.svelte
+++ myportfolio/src/lib/components/elements/Input.svelte
@@ -2,11 +2,21 @@
     export let labelText = "label";
     export let name = "title";
     export let inputType = "text";
+    export let textarea = false;
+    export let helperText = "Helper Text";
 </script>
 
 <div>
     <label for={name} class="body-1">{labelText}</label>
-    <input type={inputType} id={name} name={name}/>
+    {#if textarea}
+        <textarea id={name} name={name} rows="10"></textarea>
+        <div class="helper-text">
+            <p class="body-3">{helperText}</p>
+            <p class="body-3">0/300 words</p>
+        </div>        
+    {:else}        
+        <input type={inputType} id={name} name={name}/>
+    {/if}
 </div>
 
 
@@ -18,14 +28,31 @@
         margin-bottom: 8px;
     }
 
+    .helper-text {
+        color: var(--main-colors-gray-4);
+        display: flex;
+        justify-content: space-between;
+        margin-top: 8px;
+        margin-bottom: 16px;
+    }
+
+    textarea, input {
+        border: 1px solid var(--gray);
+        color: var(--gray);
+        font-family: 'General Sans Regular', 'Times New Roman', Times, serif;        
+        background: transparent;
+        font-size: 24px; 
+        width: 100%;
+    }
+
+    textarea {
+        padding: 32px 48px;
+        resize: none;    
+    }
+
     input {
         padding: 16px 24px;
-        border: 1px solid #676767;
-        width: 100%;
-        background: var(--white);
-        font-family: 'General Sans Regular', 'Times New Roman', Times, serif;
-        font-size: 24px;
-        font-weight: 400;
+        
     }
 
     div {
