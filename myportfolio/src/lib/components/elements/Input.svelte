919e9d93b59fcbf41e53a731b12f06487da1172c
GengineerDev <pedegloriodavidgenesis@gmail.com>
2023-10-26 14:58:17 +0800

    Add word count functionality to textarea

--- myportfolio/src/lib/components/elements/Input.svelte
+++ myportfolio/src/lib/components/elements/Input.svelte
@@ -1,61 +1,76 @@
 <script>
-    export let labelText = "label";
-    export let name = "title";
-    export let inputType = "text";
-    export let textarea = false;
-    export let helperText = "Helper Text";
+	export let labelText = 'label';
+	export let name = 'title';
+	export let inputType = 'text';
+	export let textarea = false;
+	export let helperText = 'Helper Text';
+
+	let projectDesc = '';
+	let wordCount = 0;
+
+	const onKeyup = () => {
+		let matchWords = projectDesc.match(/\S+/g);
+		wordCount = matchWords ? matchWords.length : 0;
+	};
 </script>
 
 <div>
-    <label for={name} class="body-1">{labelText}</label>
-    {#if textarea}
-        <textarea id={name} name={name} rows="10"></textarea>
-        <div class="helper-text">
-            <p class="body-3">{helperText}</p>
-            <p class="body-3">0/300 words</p>
-        </div>        
-    {:else}        
-        <input type={inputType} id={name} name={name}/>
-    {/if}
+	<label for={name} class="body-1">{labelText}</label>
+	{#if textarea}
+		<textarea id={name} {name} bind:value={projectDesc} on:keyup={onKeyup} rows="10" class="{wordCount > 300 ? 'red-border' : ''}"/>
+		<div class="helper-text">
+			<p class="body-3 {wordCount > 300 ? 'red' : ''}">{wordCount > 300 ? `Keep your message within 300 words` : helperText}</p>
+			<p class="body-3 {wordCount > 300 ? 'red' : ''}">{wordCount}/300 words</p>
+		</div>
+	{:else}
+		<input type={inputType} id={name} {name} />
+	{/if}
 </div>
 
-
 <style>
-    label {
-        font-family: unset;
-        font-weight: unset;
-        display: block;
-        margin-bottom: 8px;
+	label {
+		font-family: unset;
+		font-weight: unset;
+		display: block;
+		margin-bottom: 8px;
+	}
+
+	.helper-text {
+		color: var(--main-colors-gray-4);
+		display: flex;
+		justify-content: space-between;
+		margin-top: 8px;
+		margin-bottom: 16px;
+	}
+
+	textarea,
+	input {
+		border: 1px solid var(--gray);
+		color: var(--gray);
+		font-family: 'General Sans Regular', 'Times New Roman', Times, serif;
+		background: transparent;
+		font-size: 24px;
+		width: 100%;
+	}
+
+	textarea {
+		padding: 32px 48px;
+		resize: none;
+	}
+
+	input {
+		padding: 16px 24px;
+	}
+
+	div {
+		margin: 16px 0;
+	}
+
+    .red-border {
+        border: 1px solid var(--red_border);
     }
 
-    .helper-text {
-        color: var(--main-colors-gray-4);
-        display: flex;
-        justify-content: space-between;
-        margin-top: 8px;
-        margin-bottom: 16px;
-    }
-
-    textarea, input {
-        border: 1px solid var(--gray);
-        color: var(--gray);
-        font-family: 'General Sans Regular', 'Times New Roman', Times, serif;        
-        background: transparent;
-        font-size: 24px; 
-        width: 100%;
-    }
-
-    textarea {
-        padding: 32px 48px;
-        resize: none;    
-    }
-
-    input {
-        padding: 16px 24px;
-        
-    }
-
-    div {
-        margin: 16px 0;
-    }
+	.red {
+		color: var(--red_danger);
+	}
 </style>
