ce4c48dad91067074510be91fc228e6b3a9f7ce9
GengineerDev <pedegloriodavidgenesis@gmail.com>
2023-11-23 16:48:33 +0800

    Update Tag.svelte with no pointer for small

--- myportfolio/src/lib/components/elements/Tag.svelte
+++ myportfolio/src/lib/components/elements/Tag.svelte
@@ -14,11 +14,11 @@
 		</label>
 	</button>
 {:else if size === 'small'}
-	<button class="tag small-tag" class:tag-clicked={clicked} on:click={handleClick}>
-		<label class={clicked ? ' label-clicked' : 'gradient-text-background'}>
+	<div class="tag small-tag" >
+		<label class='gradient-text-background'>
 			<slot />
 		</label>
-	</button>
+	</div>
 {/if}
 
 <style>
@@ -37,6 +37,12 @@
 
 	.small-tag {
 		padding: 8px 16px;
+	}
+
+	.small-tag,
+	.small-tag label {
+		padding: 8px 16px;
+		cursor: default;
 	}
 
 	.tag-clicked {
